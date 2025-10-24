e8fa2d063c19dde98fabc58d0b1a2217f828f7e0
kevinCampos <kevincfj29@gmail.com>
2025-10-24 16:07:38 -0500

    24/10

--- src/LocationSkus.tsx
+++ src/LocationSkus.tsx
@@ -1,9 +1,10 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import  {data, useLocation} from 'react-router-dom'
 import { useNavigate } from 'react-router-dom';
 
-function LocationSkus() {
 
+function LocationSkus() {
+ 
 
    interface skuposicion {
      id: number,
@@ -31,6 +32,7 @@ const [posicion, setPosicion] = useState<string>("")
 const [process_id, setProcess] = useState<Number>(0)
 const [estado, setEstado] = useState<string>("")
 const [mensaje, setMensaje] = useState<string>("")
+const [error, setError] = useState<string | null>(null)
 
 useEffect(()=> {
   if(datarecibida?.sku) {
@@ -52,16 +54,51 @@ const botonconfirmar = ()  => {
 
   if(estado === 'ALMACENADO'){
    setMensaje("El SKU YA SE ENCUENTRA ALMACENADO")
+
   }
   else{
+   // if(posicion === code){
      almacenarsku();
+ /*   }
+    else {
+      setMensaje("La ubicación es incorrecta para este SKU")
+    } */
   }
 
 }
 
-const almacenarsku = () => {
+ let url : string = '';
+ const token = localStorage.getItem('token');
 
-  setMensaje("EL SKU SE ALMACENO CORRECTAMENTE")
+const almacenarsku = async() => {
+
+
+  setMensaje("EL SKU SE ALMACENO CORRECTAMENTE")        
+    url = `http://localhost:8090/skuposicion/actualizarestadoSKUALMACEN/${encodeURIComponent(sku)}`
+    try {
+
+        const response  =  await fetch(url,{
+          method: 'PUT',
+          headers: {
+            Authorization: `Bearer ${token}`,
+          }
+        });
+
+        if(!response.ok){
+         throw new Error("SKU no encontrado")
+        }
+
+        setEstado('ALMACENADO');
+       //  const data : skuposicion  =  response.json();
+      //   navigate('/LocationSkus', {state: data})
+      console.log(response.json.toString)
+        
+      }
+      catch (err){
+      setError((err as Error).message)
+      } 
+      finally {
+      }
 }
 
 const volverhome = () => {
@@ -69,7 +106,38 @@ const volverhome = () => {
 }
 
 
+//INPUT PARA EL SCANEO DEL QR
 
+const inputRef = useRef<HTMLInputElement>(null)
+const code : string = ('')
+
+useEffect(() => {
+const input = inputRef.current;
+if(!input) return;
+
+const handleChange = (e: Event) => {
+  const target = e.target as HTMLInputElement;
+  const code = target.value.trim()
+  console.log("codigo escaneado:", code);
+
+  // Aquí puedes enviar el código a tu backend
+      // fetch("/api/codigo", { method: "POST", body: JSON.stringify({ code }) });
+
+      // Limpiar el input y mantener el foco
+
+       target.value = "";
+      input.focus();
+}
+
+   input.addEventListener("change", handleChange);
+
+    // Mantener el foco inicial
+    input.focus();
+
+    return () => {
+      input.removeEventListener("change", handleChange);
+    };
+  }, []);
 
   return (
    
@@ -93,10 +161,10 @@ const volverhome = () => {
 <p className="w-full bg-background-light dark:bg-background-dark border border-black/20 dark:border-white/20 rounded-lg h-12 px-4 flex items-center text-black dark:text-white placeholder:text-black/50 dark:placeholder:text-white/50 focus:outline-none focus:ring-2 focus:ring-primary">FAMILIA: {familia}</p>
 <p className="w-full bg-background-light dark:bg-background-dark border border-black/20 dark:border-white/20 rounded-lg h-12 px-4 flex items-center text-black dark:text-white placeholder:text-black/50 dark:placeholder:text-white/50 focus:outline-none focus:ring-2 focus:ring-primary">POSICION: {posicion} </p>
 <p className="w-full bg-background-light dark:bg-background-dark border border-black/20 dark:border-white/20 rounded-lg h-12 px-4 flex items-center text-black dark:text-white placeholder:text-black/50 dark:placeholder:text-white/50 focus:outline-none focus:ring-2 focus:ring-primary">ESTADO: {estado} </p>
-
+ <input ref={inputRef} type="text" placeholder="Escanee el código aquí" autoFocus />
 </div>
 <div className="flex flex-col space-y-3">
-<button onClick={almacenarsku} className="w-full bg-primary text-white rounded-lg h-12 text-center font-bold hover:bg-primary/90 transition-colors">
+<button onClick={botonconfirmar} className="w-full bg-primary text-white rounded-lg h-12 text-center font-bold hover:bg-primary/90 transition-colors">
             Confirmar
           </button>
 <button onClick={volverhome} className="w-full bg-transparent border border-black/20 dark:border-white/20 text-black dark:text-white rounded-lg h-12 text-center font-bold hover:bg-primary/10 dark:hover:bg-primary/20 transition-colors">
